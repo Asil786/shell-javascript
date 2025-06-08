2f0f36c61b9ca406437775f11a67a9f8a2f2fbb0
Asil786 <asilshaikh123@gmail.com>
2025-06-08 19:14:53 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,15 +1,23 @@
+const { prependListener } = require("process");
 const readline = require("readline");
-
+const { exit } = require("process")
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const prompt = () => {
-  rl.question( "$ ", (answer) => {
-    if (answer === "exit 0") process.exit(0)
-    console.log(`${answer}: command not found`)
+// Uncomment this block to pass the first stage
+function prompt (){
+  rl.question("$ ", (answer) => {
+   
+    if(answer==="exit 0"){
+      exit(0);
+    }else if(answer.substring(0,4) =="echo" ){
+      console.log(answer.substring(5,answer.length));
+    }else{
+      console.log(`${answer}: command not found`)
+    }
     prompt()
-  })
+  });
 }
-prompt()
+prompt();
