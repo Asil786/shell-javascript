8b6d6f3f42e821827488f69b183f18c12de03272
Asil786 <asilshaikh123@gmail.com>
2025-06-08 19:20:14 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,23 +1,54 @@
-const { prependListener } = require("process");
 const readline = require("readline");
-const { exit } = require("process")
+
+const INVALID_ARGS = -2;
+const NOT_FOUND = -1;
+const EXIT = 0;
+const SUCCESS = 1;
+
+function exit(args) {
+  // Always exit with 0
+  process.exit(0);
+}
+
+function echo(args) {
+  console.log(args);
+  return SUCCESS;
+}
+
+function type(args) {
+  const command = args.trim();
+  if (BUILTIN_COMMANDS.hasOwnProperty(command)) {
+    console.log(`${command} is a shell builtin`);
+  } else {
+    console.log(`${command}: not found`);
+  }
+  return SUCCESS;
+}
+
+const BUILTIN_COMMANDS = Object.freeze({
+  exit: exit,
+  echo: echo,
+  type: type,
+});
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// Uncomment this block to pass the first stage
-function prompt (){
-  rl.question("$ ", (answer) => {
-   
-    if(answer==="exit 0"){
-      exit(0);
-    }else if(answer.substring(0,4) =="echo" ){
-      console.log(answer.substring(5,answer.length));
-    }else{
-      console.log(`${answer}: command not found`)
+const prompt = () => {
+  rl.question("$ ", (input) => {
+    const [cmd, ...argsArr] = input.trim().split(" ");
+    const args = argsArr.join(" ");
+
+    if (BUILTIN_COMMANDS.hasOwnProperty(cmd)) {
+      BUILTIN_COMMANDS[cmd](args);
+    } else {
+      console.log(`${cmd}: command not found`);
     }
-    prompt()
+
+    prompt();
   });
-}
+};
+
 prompt();
