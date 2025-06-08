96029f7da860e388b872cc9c336770270a448196
Asil786 <asilshaikh123@gmail.com>
2025-06-08 19:45:56 +0530

    [any message]

---
+++ app/EchoCommand.js
@@ -0,0 +1,16 @@
+const { Command } = require("./Command");
+
+class EchoCommand extends Command {
+    constructor(outputHandler) {
+        super();
+        this.outputHandler = outputHandler;
+    }
+
+    execute(args) {
+        const output = args.map(arg => arg.replace(/['"]/g, '')).join(' ');
+        this.outputHandler.write(output);
+        return { shouldContinue: true };
+    }
+}
+
+module.exports = { EchoCommand }
