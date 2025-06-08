f7ec2c03826ca90b286689b21498e275f61142ef
Asil786 <asilshaikh123@gmail.com>
2025-06-08 19:18:19 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,42 +1,23 @@
-const BUILTIN_COMMANDS = Object.freeze({
-  "exit": exit,
-  "echo": echo,
-  "type": type
+const { prependListener } = require("process");
+const readline = require("readline");
+const { exit } = require("process")
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const INVALID_ARGS = -2;
-const NOT_FOUND = -1;
-const EXIT = 0;
-const SUCCESS = 1;
-
-function exit(args) {
-  if (args == 0) {
-    return EXIT;
-  }
-  return EXIT;
-}
-
-function echo(args) {
-  console.log(args);
-  return SUCCESS;
-}
-
-function type(args) {
-  for(key of Object.keys(BUILTIN_COMMANDS)) {
-    if(key == args) {
-      console.log(`${args} is a shell builtin`);
-      return SUCCESS;
+// Uncomment this block to pass the first stage
+function prompt (){
+  rl.question("$ ", (answer) => {
+   
+    if(answer==="exit 0"){
+      exit(0);
+    }else if(answer.substring(0,4) =="echo" ){
+      console.log(answer.substring(5,answer.length));
+    }else{
+      console.log(`${answer}: command not found`)
     }
-  }
-
-  console.log(`${args}: not found`);
-  return SUCCESS;
+    prompt()
+  });
 }
-
-module.exports = {
-  BUILTIN_COMMANDS,
-  INVALID_ARGS,
-  NOT_FOUND,
-  EXIT,
-  SUCCESS
-};
+prompt();
